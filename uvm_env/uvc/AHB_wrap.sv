9b08166c72325eef64f2d5e32b777c901404ce91
psumesh <spumesh@outlook.com>
2021-08-23 20:55:11 +0530

    wrap class called in AHB_driver module

--- uvm_env/uvc/AHB_wrap.sv
+++ uvm_env/uvc/AHB_wrap.sv
@@ -1,10 +1,7 @@
-class wrap;
-
-    bit [31:0]temp_lower_boundry;
-	bit [31:0]temp_upper_boundry;
+class wraper_class;
 	
-//______________________________________________________________________________________________
-//______________________________________________________________________________________________
+//_____________________________________________________________________________________________________________________
+//_____________________________________________________________________________________________________________________
     function bit [4:0]hburst_decoder(input bit [2:0]hburst);
        case(hburst)
           3'b010  : return 4;                           // WRAP 4
@@ -13,60 +10,97 @@ class wrap;
           default : $display("NOT A VALID WRAP!!!");
        endcase
     endfunction
-//______________________________________________________________________________________________	
-//______________________________________________________________________________________________
+//_____________________________________________________________________________________________________________________	
+//_____________________________________________________________________________________________________________________
     function bit [7:0]hsize_decoder(input bit [2:0]hsize);
          case(hsize)
-            3'b000 : return 1;
-            3'b001 : return 2;
-            3'b010 : return 4;
-            3'b011 : return 8;   
-            3'b100 : return 16;
-            3'b101 : return 32;
-            3'b110 : return 64;
-            3'b111 : return 128;
+            3'b000 : return 1;          // byte
+            3'b001 : return 2;          // half word
+            3'b010 : return 4;          // word
+            3'b011 : return 8;          // double word
+            3'b100 : return 16;         // 4 word line
+            3'b101 : return 32;         // 8 word line
+            3'b110 : return 64;         // 16 word line
+            3'b111 : return 128;        // 32 word line
         endcase
     endfunction
-//______________________________________________________________________________________________
-//______________________________________________________________________________________________
+//_______________________________________________________________________________________________________________________
+//_______________________________________________________________________________________________________________________
     function bit [31:0]lower_boundry(input bit [ 2:0]hsize,
-	                               input bit [ 2:0]hburst,
-	                               input bit [31:0]haddr);						   
-        return (((haddr)/(hsize*hburst_decoder(hburst)))*((hsize*hburst_decoder(hburst))));
+	                                 input bit [ 2:0]hburst,
+	                                 input bit [31:0]haddr);						   
+        return (((haddr)/(hsize_decoder(hsize)*hburst_decoder(hburst)))*((hsize_decoder(hsize)*hburst_decoder(hburst))));
     endfunction
-//______________________________________________________________________________________________
-//______________________________________________________________________________________________
+//_______________________________________________________________________________________________________________________
+//_______________________________________________________________________________________________________________________
 	
     function bit [31:0]upper_boundry(input bit [ 2:0]hsize,
                                    input bit [ 2:0]hburst,
                                    input bit [31:0]haddr);
-        return (lower_boundry(hsize, hburst, haddr) + (hsize*hburst_decoder(hburst)));
+        return (lower_boundry(hsize, hburst, haddr) + (hsize_decoder(hsize)*hburst_decoder(hburst)));
     endfunction
-//______________________________________________________________________________________________
-//______________________________________________________________________________________________
+//_________________________________________________________________________________________________________________________
+//_________________________________________________________________________________________________________________________
 
-    task wrap_calc(input bit [ 2:0]hsize,
+     task wrap_calc(input bit [ 2:0]hsize,
                    input bit [ 2:0]hburst,
-                   input bit [31:0]haddr);
+                   input bit [31:0]haddr,
+                   output bit [31:0]queue[$]);
+        
+        bit [31:0]temp_lower_boundry;
+        bit [31:0]temp_upper_boundry;
+		bit [31:0] out_address;
     			
         temp_lower_boundry = lower_boundry(hsize, hburst, haddr);
         temp_upper_boundry = upper_boundry(hsize, hburst, haddr);
 
-        $display("%0h %0h", temp_lower_boundry, temp_upper_boundry);
+        //$display("%0h %0h", temp_lower_boundry, temp_upper_boundry);
+		
+		
+		
+		repeat((hburst_decoder(hburst) + 1)) begin
+		    if(haddr <= temp_upper_boundry) begin
+			    out_address = haddr;
+				haddr = (haddr + hsize_decoder(hsize));
+				queue.push_back(out_address);
+			end
+			
+			else if(haddr > temp_upper_boundry) begin
+			    out_address = (haddr - temp_upper_boundry);
+				haddr = temp_lower_boundry + out_address;
+			end
+		end
+		//$display("%p", queue);
+		
+		
+		
+		
     endtask
-//______________________________________________________________________________________________
-//______________________________________________________________________________________________
+//_________________________________________________________________________________________________________________________
+//_________________________________________________________________________________________________________________________
 
 endclass
 
 /*
 module test;
  wrap wrap_;
+ bit [31:0] queue[$];
  
  initial begin
     wrap_ = new();
     
-    wrap_.wrap_calc(3'h4, 3'h4, 32'h34);
+    
+	wrap_.wrap_calc(3'h2, 3'h4, 32'h34, queue);         //hsize, hburst, haddr
+    foreach(queue[i])
+	   $display("%0h", queue[i]);
+
+    $display();
+  wrap_.wrap_calc(3'h2, 3'h4, 32'h50, queue);         //hsize, hburst, haddr
+    foreach(queue[i])
+	   $display("%0h", queue[i]);
  end
+ 
+
+ 
 endmodule
 */
