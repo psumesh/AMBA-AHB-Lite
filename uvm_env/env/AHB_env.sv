b1cf3f5daaa692b4042601b6cd679f408aa91132
psumesh <spumesh@outlook.com>
2021-08-27 20:14:47 +0530

    env updated with scoreboard and subscriber

--- uvm_env/env/AHB_env.sv
+++ uvm_env/env/AHB_env.sv
@@ -6,6 +6,7 @@ class AHB_env extends uvm_env;
 	AHB_agent2 agent2;
 	
 	AHB_scoreboard scorebaord;
+	AHB_subscriber coverage1;
 	
 	
 	function new(string name = "AHB_env", uvm_component parent);
@@ -16,12 +17,14 @@ class AHB_env extends uvm_env;
 	    agent1     = AHB_agent1::type_id::create("agent1", this);
 		agent2     = AHB_agent2::type_id::create("agent2", this);
 		scorebaord = AHB_scoreboard::type_id::create("scorebaord", this);
+		coverage1  = AHB_subscriber::type_id::create("coverage1", this);
 	endfunction
 	
 	function void connect_phase(uvm_phase phase);
 		super.connect_phase(phase);
 		agent1.monitor1.item_collected_port.connect(scorebaord.mon1_rec);
 		agent2.monitor2.item_collected_port.connect(scorebaord.mon2_rec);
+		agent1.monitor1.item_collected_port.connect(coverage1.analysis_export);
 	endfunction
 
 endclass
